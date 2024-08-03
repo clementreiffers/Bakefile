87e990c4dde2263961df662fc143c7dd52898683
clementreiffers <clement.reiffers@revolve.team>
2024-08-03 14:32:46 +0200

    feat(bake): permitting including other Bakefiles

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::process::exit;
 struct Bakefile {
     variables: Vec<(String, String)>,
     rules: Vec<Rule>,
+    includes: Vec<String>,
 }
 
 #[derive(Parser, Debug)]
@@ -41,8 +42,14 @@ fn get_rule<'a>(bakefile: &'a Bakefile, target: &'a str) -> Option<&'a Rule> {
     bakefile.rules.iter().find(|rule| rule.target == target)
 }
 
-fn populate_bakefile(filename: &str, variables: &mut Vec<(String, String)>, rules: &mut Vec<Rule>) {
+fn populate_bakefile(
+    filename: &str,
+    variables: &mut Vec<(String, String)>,
+    rules: &mut Vec<Rule>,
+    includes: &mut Vec<String>,
+) {
     let mut current_target: String = String::new();
+    let mut is_including: bool = false;
 
     io::BufReader::new(File::open(filename).expect("File; not found"))
         .lines()
@@ -50,10 +57,19 @@ fn populate_bakefile(filename: &str, variables: &mut Vec<(String, String)>, rule
         .filter(|line| !line.is_empty() && !line.starts_with('#')) // Filtre les lignes vides ou commentaires
         .for_each(|line| {
             if line.starts_with(' ') || line.starts_with('\t') {
-                store_recipe(rules, &line, &current_target);
+                if is_including {
+                    includes.push(line.trim().to_string());
+                } else {
+                    store_recipe(rules, &line, &current_target);
+                }
             } else if let Some((target, dependencies)) = line.split_once(':') {
+                if target == "include" {
+                    is_including = true;
+                    return;
+                }
                 let target: String = target.trim().to_string();
                 current_target = target.clone();
+                is_including = false;
                 rules.push(Rule {
                     target,
                     dependencies: dependencies.split_whitespace().map(String::from).collect(),
@@ -64,12 +80,24 @@ fn populate_bakefile(filename: &str, variables: &mut Vec<(String, String)>, rule
             }
         });
 }
-
 fn read_bakefile(filename: &str) -> io::Result<Bakefile> {
     let mut variables = Vec::new();
     let mut rules = Vec::new();
-    populate_bakefile(filename, &mut variables, &mut rules);
-    Ok(Bakefile { variables, rules })
+    let mut includes = Vec::new();
+
+    // Initial population of the bakefile
+    populate_bakefile(filename, &mut variables, &mut rules, &mut includes);
+
+    // Collect includes that need further processing
+    while let Some(include) = includes.pop() {
+        populate_bakefile(&include, &mut variables, &mut rules, &mut includes);
+    }
+
+    Ok(Bakefile {
+        variables,
+        rules,
+        includes,
+    })
 }
 
 fn execute_command(command: String) {
@@ -123,5 +151,7 @@ fn execute_rule(bakefile: &Bakefile, target_rule: &str) {
 fn main() {
     let args: Args = Args::parse();
     let bakefile = read_bakefile("Bakefile").unwrap();
+
+    println!("{:?}", bakefile);
     execute_rule(&bakefile, &args.rule);
 }
